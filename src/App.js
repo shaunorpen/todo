681549ab919a152dd0ff7b9dab328a2ec653e7b7
Shaun Orpen <shaunorpen@Shauns-MacBook-Pro.local>
2019-10-22 17:28:49 +0100

    Add add task functionality

--- src/App.js
+++ src/App.js
@@ -6,19 +6,39 @@ const initialTodos = [
   {id: 3, description:'Learn JavaScript', due: '', isComplete: true, isSelected: false},
 ];
 
+function AddTask({todos, setTodos}) {
+  debugger
+  const [task, setTask] = React.useState('');
+  const newTask = {
+    id: 4,
+    description: task,
+    due: '',
+    isComplete: false,
+    isSelected: false,
+  }
+  return (
+    <>
+     <h2>Add To-do</h2>
+     <input type='text' name='task' value={task} onChange={e => setTask(e.target.value)} />
+     <button onClick={e => {
+       setTodos([...todos, newTask])
+       setTask('')
+       }}>Add</button>
+    </>
+  )
+}
+
 function TaskList({isComplete, todos, setTodos}) {
   const toggleSelected = id => e => {
     setTodos(todos.map(todo => todo.id === id 
       ? {...todo, isSelected: !todo.isSelected} 
       : todo))
   }
-
   const toggleComplete = () => {
     setTodos(todos.map(todo => todo.isSelected 
       ? {...todo, isComplete: !todo.isComplete, isSelected: false} 
       : todo))
   }
-
   return (
     <>
       <h2>{isComplete ? 'Done' : 'To-do'}</h2>
@@ -44,6 +64,7 @@ function App() {
     <div className="App">
       <h1>Wunderlist 2.0</h1>
       <TaskList isComplete={false} todos={todos} setTodos={setTodos} />
+      <AddTask todos={todos} setTodos={setTodos} />
       <TaskList isComplete={true}  todos={todos} setTodos={setTodos} />
     </div>
   );
